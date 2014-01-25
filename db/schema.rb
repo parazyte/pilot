e036847792b233642bc2e107c2aabd79bcf0b795
Mathias Klippinge <mathias.klippinge@gmail.com>
2014-01-25 21:56:58 +0100

    Increase system version. Oh yea.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140125143300) do
+ActiveRecord::Schema.define(:version => 20140125205555) do
 
   create_table "cms_contents", :force => true do |t|
     t.string   "main_text"
